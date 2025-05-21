cca5081e806be8c8ecfa2152f34f0e4fdc863cd2
CASician <cristian.sician@gmail.com>
2025-05-21 21:35:56 +0200

    fix favicon

--- lib/env.dart
+++ lib/env.dart
@@ -9,3 +9,32 @@ class Env {
   static const Color secondaryColor = Color(0xFFf9a03f);
   static const Color textColor = Color(0xFF0b2b26);
 }
+
+/*
+# I colori di Berna
+# PRIMARY_COLOR=0xFF8bc34a
+# SECONDARY_COLOR=0xFFffac1c
+
+# TODO
+# [x] Colonna cliccabile
+# [x] Filtro
+# [x] Pagina di lascia mail e cap
+# [x] Social share sulla landing
+
+# [x] config. telefono
+# [x] cambiare lead
+# [x] logo
+# [x] piatto veg
+# [x] colori filtri
+# [x] cambio font
+# [ ] cambia nome piatti
+# [ ] bottone condividi amici
+# [x] logo ig
+# [x] google sheets
+# [x] API google sheets
+# [x] bottone personalizza al grammo
+# [ ] deploy
+# [ ] codice qr
+
+# [ ] tasto vai
+ */
